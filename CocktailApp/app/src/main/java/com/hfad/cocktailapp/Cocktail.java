0188647722c64481dc7d768b447f61f2881d6049
Matthew Duffin <matthewtduffin@gmail.com>
2016-07-28 13:13:10 -0700

    initial commit

---
+++ CocktailApp/app/src/main/java/com/hfad/cocktailapp/Cocktail.java
@@ -0,0 +1,20 @@
+package com.hfad.cocktailapp;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by matthewtduffin on 28/07/16.
+ */
+public class Cocktail {
+    String name, author, category;
+    List<String> methods;
+    List<String[]> ingredients;
+
+    public Cocktail() {
+
+    }
+
+
+
+}
